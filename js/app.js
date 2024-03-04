4d5378627f24d884fbcb4bc2d88569b8ec773e79
Gleb Svetogor <asabsiu@mail.ru>
2024-03-04 19:09:35 +0300

    Deleted popover targets and actions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
-var registratinoBtn = document.getElementById("signup-btn");
-var registrationWindow = document.getElementById("signup-window");
-var loginBtn = document.getElementById("login-btn");
-var loginWindow = document.querySelector("#login-window");
-var userBtn = document.querySelector("#user-btn");
-
-user-btn.addEventListener('click', () => {
-    loginWindow.style.display = loginWindow.style.display === 'none' ? 'block' : 'none';
-});
+document.querySelector('#user-btn').addEventListener('click', function (e) {
+    var div = document.querySelector('#login-window')
+    div.style.display = div.style.display === 'none' ? 'block' : 'none'
+})
