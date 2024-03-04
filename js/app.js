9bdb81d6d2bd0300d6642025998f8ea23e1118a1
Gleb Svetogor <asabsiu@mail.ru>
2024-03-05 01:47:24 +0300

    Added signup window and event hadnler for this window

--- js/app.js
+++ js/app.js
@@ -1,15 +1,63 @@
 const loginWindow = document.querySelector(".window--login");
 const loginBtn = document.querySelector(".header__heading-user");
+const loginWindowSignupBtn = document.querySelector(".window__signup-btn");
 const closeLoginBtn = document.querySelector(".window__close-btn--login");
 
+const signupWindow = document.querySelector(".window--signup");
+const signupWindowLoginBtn = document.querySelector(".window__login-btn");
+const closeSignupBtn = document.querySelector(".window__close-btn--signup");
 
+closeLoginBtn.addEventListener('click',() => {
+    const result = getClassNameFromElement(loginWindow);
+    windowVisibility(result);
+});
 
-loginBtn.addEventListener('click',openLoginWindow);
+loginBtn.addEventListener('click', () => {
+    const result = getClassNameFromElement(loginWindow);
+    windowVisibility(result);
+});
 
-function openLoginWindow(){
-    loginWindow.style.display = "flex";
+loginWindowSignupBtn.addEventListener('click',() => {
+    const resultSignup = getClassNameFromElement(signupWindow);
+    const resultLogin = getClassNameFromElement(loginWindow);
+    windowVisibility(resultLogin);
+    windowVisibility(resultSignup);
+});
+
+closeSignupBtn.addEventListener('click', () => {
+    const result = getClassNameFromElement(signupWindow);
+    windowVisibility(result);
+});
+
+signupWindowLoginBtn.addEventListener('click', () => {
+    const resultSignup = getClassNameFromElement(signupWindow);
+    const resultLogin = getClassNameFromElement(loginWindow);
+    windowVisibility(resultLogin);
+    windowVisibility(resultSignup);
+})
+
+function windowVisibility(elementClassName) {
+    var el = document.querySelector(elementClassName);
+    if(el){
+        const computedStyle = window.getComputedStyle(el);
+        const displayValue = computedStyle.getPropertyValue('display');
+        console.log(elementClassName);
+        switch (displayValue){
+            case "none":
+                el.style.display = "flex";
+                break;
+            case "flex":
+                el.style.display = "none";
+                break;
+            default:
+                console.log("element not found");
+        }
+    }
 }
 
-function closeLoginWindow(){
-    loginWindow.style.display = "none";
+function getClassNameFromElement(el){
+    const inputString = el.className;
+    const startIndex = inputString.indexOf(" ") + 1;
+    const result = "." + inputString.substring(startIndex);
+    return result;
 }
--- js/swiper.js
+++ js/swiper.js
@@ -3,4 +3,9 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    autoplay: {
+      delay: 5000,
+    },
+    speed: 3000,
+    loop: true,
   });
