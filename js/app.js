e77b80a2009e9a2123db39c52bc6e3f5d33f7715
Gleb Svetogor <asabsiu@mail.ru>
2024-03-05 00:21:31 +0300

    Added login window with event hadler

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
-document.querySelector('#user-btn').addEventListener('click', function (e) {
-    var div = document.querySelector('#login-window')
-    div.style.display = div.style.display === 'none' ? 'block' : 'none'
-})
+const loginWindow = document.querySelector(".window--login");
+const loginBtn = document.querySelector(".header__heading-user");
+const closeLoginBtn = document.querySelector(".window__close-btn--login");
+
+
+
+loginBtn.addEventListener('click',openLoginWindow);
+
+function openLoginWindow(){
+    loginWindow.style.display = "flex";
+}
+
+function closeLoginWindow(){
+    loginWindow.style.display = "none";
+}
