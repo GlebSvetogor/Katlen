a0953ae3f0b153bc5507311df8e40fc0c4be978d
Gleb Svetogor <asabsiu@mail.ru>
2024-03-09 14:28:50 +0300

    Added inputs in filter dropdown menu

--- js/dropdownFilter.js
+++ js/dropdownFilter.js
@@ -1,8 +1,9 @@
 const dropdownButton = document.querySelector(".catalog__list-content");
+const catalogLine = document.querySelector(".catalog__line");
 
 dropdownButton.addEventListener('click', ()=>{
     const dropdownMenu = document.querySelector(".dropdown__list");
-    console.log(dropdownMenu);
     dropdownMenu.classList.toggle("dropdown__list--active");
+    catalogLine.classList.toggle("catalog__line--active");
 });
 
