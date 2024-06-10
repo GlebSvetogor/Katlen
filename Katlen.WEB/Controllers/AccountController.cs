973c5becc4f57cfa4a1407846dc09fe6b668f15a
Gleb Svetogor <asabsiu@mail.ru>
2024-06-10 11:04:18 +0300

    Add jquery ajax script for register form

--- Katlen.WEB/Controllers/AccountController.cs
+++ Katlen.WEB/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Katlen.DAL.Entities;
 using Katlen.WEB.ViewModels;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,11 +18,58 @@ namespace Katlen.WEB.Controllers
         {
             this.db = db;
         }
-        [HttpGet]
-        public IActionResult Login()
+        [Authorize]
+        public IActionResult Index()
         {
-            return View();
+            return Content(User.Identity.Name);
         }
+        //[HttpGet]
+        //public IActionResult Register() 
+        //{
+        //    return PartialView("_Register", new RegisterModel());
+        //}
+        
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return PartialView("_SuccessMessage", model);
+            }
+            else
+            {
+                ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+                return PartialView("_Register", model);
+            }
+            //if (ModelState.IsValid)
+            //{
+            //    User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+            //    if (user == null)
+            //    {
+            //        db.Users.Add(new DAL.Entities.User { Phone = model.Phone, Name = model.Name, Email = model.Email, Password = model.Password });
+            //        await db.SaveChangesAsync();
+
+            //        await Authenticate(model.Email);
+
+            //        return RedirectToAction("Index", "Home");
+            //    }
+            //    else
+            //        ModelState.AddModelError("", "Некорректные логин и(или) пароль");
+            //}
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookid", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+        
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel model)
@@ -38,44 +86,6 @@ namespace Katlen.WEB.Controllers
                 ModelState.AddModelError("", "Некорректные логин и (или) пароль");
             }
             return View(model);
-        }
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(RegisterModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
-                if (user == null)
-                {
-                    db.Users.Add(new DAL.Entities.User { Phone = model.Phone ,Name = model.Name ,Email = model.Email, Password = model.Password });
-                    await db.SaveChangesAsync();
-
-                    await Authenticate(model.Email);
-
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                    ModelState.AddModelError("", "Некорректные логин и(или) пароль");
-            }
-            return View(model);
-        }
-
-        private async Task Authenticate(string userName)
-        {
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
-            };
-
-            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookid", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
-        
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
         }
 
         public async Task<IActionResult> Logout()
