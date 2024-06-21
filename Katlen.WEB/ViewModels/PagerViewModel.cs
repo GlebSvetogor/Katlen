fcdd282aa377222a6cf3983b6a3acf04b1aa244e
Gleb Svetogor <asabsiu@mail.ru>
2024-06-22 01:39:09 +0300

    Correct ajax technology in catalog

--- Katlen.WEB/ViewModels/PagerViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Katlen.WEB.ViewModels
-{
-    public class PagerViewModel
-    {
-        public PageViewModel Page { get; set; }
-        public FiltrViewModel Filtr { get; set; }
-    }
-}
